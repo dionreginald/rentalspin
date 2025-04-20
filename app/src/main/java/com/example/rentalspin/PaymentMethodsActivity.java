ca52280ec96e9735f04d84de6fd92cda8de340fa
dionreginald <diondexter.reginald@gmail.com>
2025-04-20 11:27:35 +0530

    Create PaymentMethodsActivity.java

---
+++ app/src/main/java/com/example/rentalspin/PaymentMethodsActivity.java
@@ -0,0 +1,13 @@
+package com.example.rentalspin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PaymentMethodsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_methods);
+    }
+}
